d4a64f3056636516a7d148b7a068299e02185e21
Filip Mieszkowski <felipe336336@gmail.com>
2021-01-10 15:54:22 +0100

    upgrade

--- GLR/GLR/settings.py
+++ GLR/GLR/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Images',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- GLR/Images/views.py
+++ GLR/Images/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def v1(request):
+    print(request)
+    return render(request, "Images/base.html")
